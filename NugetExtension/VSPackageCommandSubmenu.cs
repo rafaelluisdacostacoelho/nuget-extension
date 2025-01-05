c43f04715d0f32ac53c156a509ffb1c50cafa8e4
Rafael Luis da Costa Coelho <rafaelluisdacostacoelho@gmail.com>
2025-01-05 20:52:59 -0300

    Add a menu template

---
+++ NugetExtension/VSPackageCommandSubmenu.cs
@@ -0,0 +1,29 @@
+﻿using Microsoft.VisualStudio.Shell;
+using NugetExtension.Commands;
+using System;
+using System.Diagnostics.CodeAnalysis;
+using System.Runtime.InteropServices;
+
+namespace NugetExtension
+{
+    [PackageRegistration(UseManagedResourcesOnly = true)]
+   [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+   [Guid(VSPackageCommandSubmenu.PackageGuidString)]
+   [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+   [ProvideMenuResource("Menus.ctmenu", 1)]
+   public sealed class VSPackageCommandSubmenu : Package
+   {
+      public const string PackageGuidString = "d2cc7f26-42fb-4c8d-858f-05f3fc3599d5";
+
+      public VSPackageCommandSubmenu()
+      {
+      }
+
+      protected override void Initialize()
+      {
+         base.Initialize();
+         Command1.Initialize(this);
+         Command2.Initialize(this);
+      }
+   }
+}
