c4a087a0589ff78ec4fb0a6637a93db737d20706
Rafael Luis da Costa Coelho <rafaelluisdacostacoelho@gmail.com>
2025-01-05 22:33:48 -0300

    Change Package to AsyncPackage

--- NugetExtension/VSPackageCommandSubmenu.cs
+++ NugetExtension/VSPackageCommandSubmenu.cs
@@ -3,27 +3,34 @@ using NugetExtension.Commands;
 using System;
 using System.Diagnostics.CodeAnalysis;
 using System.Runtime.InteropServices;
+using System.Threading;
+using Task = System.Threading.Tasks.Task;
 
 namespace NugetExtension
 {
-    [PackageRegistration(UseManagedResourcesOnly = true)]
-   [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
-   [Guid(VSPackageCommandSubmenu.PackageGuidString)]
-   [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
-   [ProvideMenuResource("Menus.ctmenu", 1)]
-   public sealed class VSPackageCommandSubmenu : Package
-   {
-      public const string PackageGuidString = "d2cc7f26-42fb-4c8d-858f-05f3fc3599d5";
+    [PackageRegistration(UseManagedResourcesOnly = true, AllowsBackgroundLoading = true)]
+    [InstalledProductRegistration("#110", "#112", "1.0", IconResourceID = 400)] // Info on this package for Help/About
+    [Guid(VSPackageCommandSubmenu.PackageGuidString)]
+    [SuppressMessage("StyleCop.CSharp.DocumentationRules", "SA1650:ElementDocumentationMustBeSpelledCorrectly", Justification = "pkgdef, VS and vsixmanifest are valid VS terms")]
+    [ProvideMenuResource("Menus.ctmenu", 1)]
+    public sealed class VSPackageCommandSubmenu : AsyncPackage
+    {
+        public const string PackageGuidString = "d2cc7f26-42fb-4c8d-858f-05f3fc3599d5";
 
-      public VSPackageCommandSubmenu()
-      {
-      }
+        public VSPackageCommandSubmenu()
+        {
+        }
 
-      protected override void Initialize()
-      {
-         base.Initialize();
-         Command1.Initialize(this);
-         Command2.Initialize(this);
-      }
-   }
+        protected override async Task InitializeAsync(CancellationToken cancellationToken, IProgress<ServiceProgressData> progress)
+        {
+            await base.InitializeAsync(cancellationToken, progress);
+
+            // When initialized asynchronously, we *may* be on a background thread at this point.
+            // Do any initialization that requires the UI thread after switching to the UI thread.
+            // Otherwise, remove the switch to the UI thread if you don't need it.
+            await this.JoinableTaskFactory.SwitchToMainThreadAsync(cancellationToken);
+            Command1.Initialize(this);
+            Command2.Initialize(this);
+        }
+    }
 }
